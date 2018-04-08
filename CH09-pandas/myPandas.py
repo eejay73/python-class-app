cb57cc6d3ce467b5861028a6fc772c725f3e1742
eejay73 <coachejay@gmail.com>
2018-04-08 19:02:32 +0000

    Chapter 9 Pandas added

---
+++ CH09-pandas/myPandas.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env/ python3
+"""
+Ch09 - numpy and pandas exploration
+@author Ellery Penas
+@version 2018.04.07
+"""
+import pandas
+
+
+def main():
+    """main line function"""
+    df = pandas.DataFrame([[1, 2, 3], [20, 30, 40]])
+    print(df)
+
+
+if __name__ == "__main__":
+    main()
